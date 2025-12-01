aa8c0b4dd368cfad45cf08eb900dace8e82c5f4f
Miguel Brito <antoniommoutinho22@gmail.com>
2025-12-01 16:29:14 -0400

    atividade 02

--- FPF/repeticao_atividade02.py
+++ FPF/repeticao_atividade02.py
@@ -34,3 +34,32 @@ print("quantidade de vogais: ", qntd_vogais)
 print("quantidade de consoantes: ", qntd_consoantes)
 print("quantidade de espacos: ", qntd_espaco)
 
+#questao 3
+
+quantidade_funcionario = int(input("digite quantos funcionarios deseja adicionar: "))
+
+funcionarios = []
+
+for i in range(quantidade_funcionario):
+
+    funcionario_novo = {}
+
+    print(f"{i+1}¨ funcionario")
+    funcionario_novo["nome"] = input("digite seu nome: ")
+    funcionario_novo["sexo"] = input("digite seu sexo (M / F): ").lower()
+    funcionario_novo["salario"] = float(input("digite seu salario: "))
+
+    funcionarios.append(funcionario_novo)
+
+soma_homem = 0
+soma_mulher = 0
+
+for funcionario in funcionarios:
+    if funcionario["sexo"] == 'm':
+        soma_homem += funcionario["salario"]
+    elif funcionario["sexo"] == 'f':
+        soma_mulher += funcionario["salario"]
+
+print("\ntotal de funcionarios adicionados foi de: ", quantidade_funcionario)
+print("a soma total dos salarios homens é: ",soma_homem)
+print("a soma total dos salarios mulheres é: ",soma_mulher)
